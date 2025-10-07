a42486ddbb10daa1efd939805e57a2bf56656b07
Sajjad Hossain <sajjadhossain44371@gmail.com>
2025-10-07 20:50:12 +0600

    project setup finished

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Navbar from "./components/Header/Navbar";
+
+function App() {
+  return (
+    <>
+      <h1>App JSX</h1>
+      <Navbar></Navbar>
+    </>
+  );
+}
+
+export default App;
